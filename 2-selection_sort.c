bdeb47c3cc81e0dac315f21f073f9983e76d1dab
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-07-25 22:00:20 +0200

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -5,10 +5,11 @@
  * @a: The first integer to swap.
  * @b: The second integer to swap.
  */
-void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+void swap(int *a, int *b)
+{
+int temp = *a;
+*a = *b;
+*b = temp;
 }
 
 /**
@@ -19,31 +20,31 @@ void swap(int *a, int *b) {
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j;
-    size_t min;
-    size_t swapped = 0;
+size_t i, j;
+size_t min;
+size_t swapped = 0;
 
-    if (!array || size <= 1)
-        return;
+if (!array || size <= 1)
+return;
 
-    for (i = 0; i < size; i++)
-    {
-        min = i;
-        swapped = 0;
+for (i = 0; i < size; i++)
+{
+min = i;
+swapped = 0;
 
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min])
-            {
-                min = j;
-                swapped = 1;
-            }
-        }
+for (j = i + 1; j < size; j++)
+{
+if (array[j] < array[min])
+{
+min = j;
+swapped = 1;
+}
+}
 
-        if (swapped == 1)
-        {
-            swap(&array[i], &array[min]);
-            print_array(array, size);
-        }
-    }
+if (swapped == 1)
+{
+swap(&array[i], &array[min]);
+print_array(array, size);
+}
+}
 }
